f04d3ef9fdf5b7c46fb78d9db149dd959791b42b
jhsd <jan-henrik.schmidt-dencker@frm2.tum.de>
2019-01-18 17:26:10 +0100

    formatted

--- cesar136/CeasarCommunication.py
+++ cesar136/CeasarCommunication.py
@@ -3,40 +3,45 @@
 # def split_comstring(seq, length=8):
 #     return [seq[i:i+length] for i in range(0, len(seq), length)]
 
-CesarSerialNumber = 1      # defined by the Cesar unit used
+CesarSerialNumber = 1  # defined by the Cesar unit used
+
 
 def XorBinaries(ListOfBinaries):
     bin1 = ListOfBinaries[0]
-    for k in range(len(ListOfBinaries)-1):
+    for k in range(len(ListOfBinaries) - 1):
         binTemp = ""
-        bin2=ListOfBinaries[k+1]
-        if len(bin1)!=len(bin2):
+        bin2 = ListOfBinaries[k + 1]
+        if len(bin1) != len(bin2):
             print("Cannot compare binaries of different format")
         else:
             for l in range(len(bin1)):
-                if bin1[l]==bin2[l]:
-                    x="0"
+                if bin1[l] == bin2[l]:
+                    x = "0"
                 else:
-                    x="1"
-                binTemp+=x
-        bin1=binTemp
+                    x = "1"
+                binTemp += x
+        bin1 = binTemp
     return binTemp
+
 
 def binString(Integer):
     return bin(Integer).split("b")[1]
 
+
 def IntToBytearray(Int):
-    result=None
+    result = None
     k = 0
-    while result==None:
+    while result == None:
         try:
             result = bytearray(Int.to_bytes(k, "little"))
         except:
             k += 1
     return result
 
+
 def createEightBitBin(int):
-        return binString(int).zfill(8)
+    return binString(int).zfill(8)
+
 
 class MessagePacket():
 
@@ -46,16 +51,16 @@ class MessagePacket():
         self.ByteArray = ByteArray
         # every element for the message has two different entries( int and bin)
         # should be accessed and written to this order: [Int, Bin]
-        self.Header = [None]*2
-        self.CommandNumber = [None]*2
-        self.OptionalLengthByte = [None]*2
-        self.Checksum = [None]*2
+        self.Header = [None] * 2
+        self.CommandNumber = [None] * 2
+        self.OptionalLengthByte = [None] * 2
+        self.Checksum = [None] * 2
         self.IntBinArray = [None] * 2
         # Data is stored in a byte and a bin array with little endian, first component is the actual int value:
         # [int,[byte,byte,byte],[bin,bin,bin]
-        self.Data = [None]*3
+        self.Data = [None] * 3
         self.lenData = 0
-        self. DataLong = False
+        self.DataLong = False
 
     def splitUpPacket(self):
         if self.ByteArray:
@@ -68,49 +73,49 @@ class MessagePacket():
                 self.DataLong = True
                 self.OptionalLengthByte[0] = self.ByteArray.pop(0)
             self.Checksum[0] = self.ByteArray.pop()
-            self.lenData=len(self.ByteArray)
+            self.lenData = len(self.ByteArray)
             self.Data[1] = self.ByteArray
-            self.Data[0]=int.from_bytes(self.ByteArray,byteorder="little")
+            self.Data[0] = int.from_bytes(self.ByteArray, byteorder="little")
             self.createIntBinArray()
 
     def calculateChecksum(self):
-        self.Checksum[1]=XorBinaries(self.IntBinArray[1])
-        self.Checksum[0]=int(self.Checksum[1],2)
+        self.Checksum[1] = XorBinaries(self.IntBinArray[1])
+        self.Checksum[0] = int(self.Checksum[1], 2)
         for k in range(2):
             self.IntBinArray[k].append(self.Checksum[k])
 
     def createIntBinArray(self):
         #  due to different structure self.Header bin is calculated in self.createHeader()
-        self.CommandNumber[1]= createEightBitBin(self.CommandNumber[0])
+        self.CommandNumber[1] = createEightBitBin(self.CommandNumber[0])
         for k in range(2):
             self.IntBinArray[k] = [self.Header[k], self.CommandNumber[k]]
             if self.DataLong:
-                self.OptionalLengthByte[1]= createEightBitBin(self.OptionalLengthByte[0])
+                self.OptionalLengthByte[1] = createEightBitBin(self.OptionalLengthByte[0])
                 self.IntBinArray[k].append(self.OptionalLengthByte[k])
-            if self.lenData !=0:
+            if self.lenData != 0:
                 #
                 self.Data[2] = [createEightBitBin(kl) for kl in self.Data[1]]
-                self.IntBinArray[k]+= self.Data[k+1]
+                self.IntBinArray[k] += self.Data[k + 1]
 
-    def createHeader(self,datalength,serialnumber):
-        serialBinString=binString(serialnumber).zfill(5)
+    def createHeader(self, datalength, serialnumber):
+        serialBinString = binString(serialnumber).zfill(5)
         datalengthBinstring = binString(datalength).zfill(3)
-        self.Header[1]= serialBinString+datalengthBinstring
-        self.Header[0]=int(self.Header[1],2)
+        self.Header[1] = serialBinString + datalengthBinstring
+        self.Header[0] = int(self.Header[1], 2)
 
     def createMessagePacket(self, CommandInput, DataInput=-1):
-        self.CommandNumber[0]=int(CommandInput)
-        self.Data[0]=int(DataInput)
+        self.CommandNumber[0] = int(CommandInput)
+        self.Data[0] = int(DataInput)
         if DataInput != -1:
             self.Data[1] = IntToBytearray(self.Data[0])
             # TODO self.lenData auch in Split up
-            self.lenData=len(self.Data[1])
+            self.lenData = len(self.Data[1])
             if self.lenData > 6:
                 self.DataLong = True
                 self.OptionalLengthByte[0] = self.lenData
-                self.createHeader(7,CesarSerialNumber)
+                self.createHeader(7, CesarSerialNumber)
         if self.lenData < 7:
-            self.createHeader(self.lenData,CesarSerialNumber)
+            self.createHeader(self.lenData, CesarSerialNumber)
         self.assembleMessagePacket()
         return self.IntBinArray
 
@@ -118,24 +123,24 @@ class MessagePacket():
         # assembles every component of the message, calculates the Checksum and
         self.createIntBinArray()
         self.calculateChecksum()
-        self.ByteArray=bytearray(self.IntBinArray[0])
+        self.ByteArray = bytearray(self.IntBinArray[0])
 
 
 class ReceivedByteArray(MessagePacket):
 
-    def __init__(self,ByteArray):
-        MessagePacket.__init__(self,ByteArray)
+    def __init__(self, ByteArray):
+        MessagePacket.__init__(self, ByteArray)
         MessagePacket.splitUpPacket(self)
 
-    def extractData(self,DataConfig):
-        self.formatedData=[]
-        self.DataRanges=DataConfig[0]
+    def extractData(self, DataConfig):
+        self.formatedData = []
+        self.DataRanges = DataConfig[0]
         self.DataRangesFormat = DataConfig[1]
-        lastByte=0
+        lastByte = 0
         for k in range(len(self.DataRanges)):
             if self.DataRangesFormat[k] == 0:
                 # data format is an integer
-                data = int.from_bytes(self.Data[1][lastByte:self.DataRanges[k]],byteorder="little")
+                data = int.from_bytes(self.Data[1][lastByte:self.DataRanges[k]], byteorder="little")
             elif self.DataRangesFormat[k] == 1:
                 # data format is a certain code
                 pass
@@ -143,28 +148,27 @@ class ReceivedByteArray(MessagePacket):
                 # dataformat is a string
                 data = self.Data[1][lastByte:self.DataRanges[k]].decode()
             self.formatedData.append(data)
-            lastByte=self.DataRanges[k]
+            lastByte = self.DataRanges[k]
 
     def checkForCompletness(self):
         self.Checksum[1] = createEightBitBin(self.Checksum[0])
         self.IntBinArray[1].append(self.Checksum[1])
         result = 0
-        XorBin=XorBinaries(self.IntBinArray[1])
+        XorBin = XorBinaries(self.IntBinArray[1])
         for r in XorBin:
-            result+=int(r)
+            result += int(r)
         return result
 
     def getCesarAddress(self):
         # The address of the Unit to talk to and talking to the host is in the 4th to \
         # 8th bit of the header
         Binadress = self.Header[1][0:5]
-        return int(Binadress,2)
-
+        return int(Binadress, 2)
 
 # x=MessagePacket()
 # r=x.createMessagePacket(1)
 # s=MessagePacket()
 # f=s.createMessagePacket(129)
-#kl=MessagePacket().createMessagePacket(125)
+# kl=MessagePacket().createMessagePacket(125)
 #
-#print(kl)
+# print(kl)
--- cesar136/commands.py
+++ cesar136/commands.py
@@ -5,30 +5,29 @@ class commands():
     def __init__(self, commandNumber, DataBytesTosend, DatabytesExpected, DataConfig=None):
         self.commandNumber = commandNumber
         self.DataBytesToSend = DataBytesTosend
-        self.DataBytesExpected=DatabytesExpected
-        if self.commandNumber <128:
+        self.DataBytesExpected = DatabytesExpected
+        if self.commandNumber < 128:
             self.CSRonly = True
         else:
             self.CSRonly = False
             self.DataConfig = DataConfig
 
-
-    def set_data(self,data):
-        self.data=data
+    def set_data(self, data):
+        self.data = data
 
     def prepareInteraction(self):
         if self.DataBytesToSend != 0:
-            self.intBinArray=MessagePacket().createMessagePacket(self.commandNumber,self.data)
+            self.intBinArray = MessagePacket().createMessagePacket(self.commandNumber, self.data)
         else:
-            self.intBinArray=MessagePacket().createMessagePacket(self.commandNumber)
+            self.intBinArray = MessagePacket().createMessagePacket(self.commandNumber)
 
 
-turnOutputOff = commands(1,0,1)
+turnOutputOff = commands(1, 0, 1)
 
-reportPowerSupplyType = commands(128,0,5,[[5],[2]])
-reportModelNumber = commands(129,0,5,[[5],[0]])
-reportRFRampOnOff = commands(151,0,4,[[2,4],[0,0]])
+reportPowerSupplyType = commands(128, 0, 5, [[5], [2]])
+reportModelNumber = commands(129, 0, 5, [[5], [0]])
+reportRFRampOnOff = commands(151, 0, 4, [[2, 4], [0, 0]])
 
-reportReflectedPowerParameters = commands(152,0,3,[[1,3],[0,0]])
-reportRegulationMode = commands(154,0,1,[[1],[1]])
-reportActiveControlMode = commands(155,0,1,[[1],[1]])
+reportReflectedPowerParameters = commands(152, 0, 3, [[1, 3], [0, 0]])
+reportRegulationMode = commands(154, 0, 1, [[1], [1]])
+reportActiveControlMode = commands(155, 0, 1, [[1], [1]])
