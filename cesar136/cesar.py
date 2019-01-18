d05cdbb06ec02eecc57e132d011aa495e0088ad2
Jan-Henrik <jan-henrik.schmidt-dencker@frm2.tum.de>
2019-01-18 17:03:51 +0100

    initial commit

---
+++ cesar136/cesar.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2016, see AUTHORS.md
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from e21_util.pvd.transport import Serial
+
+def get_transport():
+     return Serial('/dev/ttyUSB12', 19200, 8, 'O', 1, 1)
+
