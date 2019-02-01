6e806ebbe9795be11fb8b55c30228490ee03913f
Alexander Book <alexander.book@frm2.tum.de>
2019-02-01 13:32:37 +0100

    removing import

--- cesar136/cesar.py
+++ cesar136/cesar.py
@@ -13,8 +13,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from e21_util.pvd.transport import Serial
-
 from e21_util.pvd.connection import Connection
 from e21_util.pvd.devices import Devices
 
