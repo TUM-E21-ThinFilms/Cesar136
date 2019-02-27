25ee8bb5f37cb1e2c378074bdeedec4cd6e09cd3
Alexander Book <alexander.book@frm2.tum.de>
2019-02-27 10:38:48 +0100

    removing import

--- cesar136/io_data.py
+++ cesar136/io_data.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from cesar136.constants import ParamInfos, Parameter
+from cesar136.constants import Parameter
 from cesar136.parameter import AbstractParameter
 
 
