728df2273c93a1769615c3420a22adaa79ee5938
Alexander Book <alexander.book@frm2.tum.de>
2019-02-27 10:40:14 +0100

    adding typing import

--- cesar136/command.py
+++ cesar136/command.py
@@ -13,6 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from typing import List
 from e21_util.interface import Loggable
 
 from cesar136.message import MessagePacket
